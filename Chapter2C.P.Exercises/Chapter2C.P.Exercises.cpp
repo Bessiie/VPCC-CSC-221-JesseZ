7a5b263297a146b5398e5868f2ff4fef846b42ff
Jesse zheng <BessiieBoi@outlook.com>
2025-02-05 18:42:23 -0500

    Added Comments

--- Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
+++ Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
@@ -1,6 +1,7 @@
 // Chapter2C.P.Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Jesse Zheng
+//Feburary 5 2025
+//IDE and VCS Setup Project
 #include <iostream>
 
 int main()
