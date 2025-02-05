ad030d2f26a111414a0638923b6f2afbd480db45
Jesse zheng <BessiieBoi@outlook.com>
2025-02-05 18:28:47 -0500

    Checkpoint 2.17

--- Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
+++ Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
@@ -1,9 +1,20 @@
-// Chapter2C.P.Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Jesse Zheng
+//Feburary 5 2025
+//Chapter 2 Exercises
+//Checkpoint 2.17
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name, address, phone;
+	name = "Jesse Zheng";
+	address = "1600 Pennsylvania Avenue NW, Washington, DC";
+	phone = "123-123-1234";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phone << endl;
+	return 0;
 }
