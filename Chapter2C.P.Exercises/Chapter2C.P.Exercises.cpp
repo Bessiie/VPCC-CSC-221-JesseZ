9aa8984eb820743a8aa13fbdd859149a9bf80d57
Jesse zheng <BessiieBoi@outlook.com>
2025-02-05 18:00:53 -0500

    Chapter 2.14  Name intials

--- Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
+++ Chapter2C.P.Exercises/Chapter2C.P.Exercises.cpp
@@ -1,9 +1,19 @@
 // Chapter2C.P.Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+//Jesse Zheng
+//Feburary 5 2025
+//Chapter 2 Exercises
+//Checkpoint 2.14
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char first, middle, last;
+    first = 'J';
+    middle = ' ';
+    last = 'Z';
+    cout << first << middle << last;
+    return 0;
 }
